56fa4673d8b9a3ae47700006b542e9eb9f458697
Jian Furquim <furquimjian@gmail.com>
2024-10-31 11:27:30 -0300

    Using vitest coverage

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -45,7 +45,7 @@ describe('Register Service', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerService.execute({
         name: 'John Doe',
         email,
